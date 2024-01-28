2de06dda1bc7cac86a76964aa832bb29075a96c8
Michael J. Fitzgerald <michael.fitzgerald.1406@gmail.com>
2024-01-28 13:50:59 +0900

    small bug

--- src/server/api/routers/didgya.ts
+++ src/server/api/routers/didgya.ts
@@ -3,18 +3,22 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 
 export const didgyaRouter = createTRPCRouter({
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+  // TODO: Create Didgya
+  // create: publicProcedure
+  //   .input(z.object({ 
+  //     name: z.string().min(1)
 
-      return ctx.db.didgya.create({
-        data: {
-          name: input.name,
-        },
-      });
-    }),
+  //   }))
+  //   .mutation(async ({ ctx, input }) => {
+  //     // simulate a slow db call
+  //     await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  //     return ctx.db.didgya.create({
+  //       data: {
+  //         name: input.name,
+  //       },
+  //     });
+  //   }),
 
   getLatest: publicProcedure.query(({ ctx }) => {
     return ctx.db.didgya.findFirst({
