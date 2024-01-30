9aa5508ab2bc563fc7dd680949c9c264f701f0d7
Michael J. Fitzgerald <michael.fitzgerald.1406@gmail.com>
2024-01-30 21:26:39 +0900

    handle create later

--- src/server/api/routers/didgya.ts
+++ src/server/api/routers/didgya.ts
@@ -4,21 +4,21 @@ import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 
 export const didgyaRouter = createTRPCRouter({
   // TODO: Create Didgya
-  create: publicProcedure
-    .input(z.object({ 
-      name: z.string().min(1)
+  // create: publicProcedure
+  //   .input(z.object({ 
+  //     name: z.string().min(1)
 
-    }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+  //   }))
+  //   .mutation(async ({ ctx, input }) => {
+  //     // simulate a slow db call
+  //     await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      return ctx.db.didgya.create({
-        data: {
-          name: input.name,
-        },
-      });
-    }),
+  //     return ctx.db.didgya.create({
+  //       data: {
+  //         name: input.name,
+  //       },
+  //     });
+  //   }),
 
   getLatest: publicProcedure.query(({ ctx }) => {
     return ctx.db.didgya.findFirst({
